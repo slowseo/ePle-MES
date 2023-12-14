a416d9744adbf47e190c8feae8c719033c76a462
J2Y98 <qjadj98@gmail.com>
2023-12-14 17:51:38 +0900

    설비 컨트롤러 갱신

--- src/main/java/com/itwillbs/controller/FacilityController.java
+++ src/main/java/com/itwillbs/controller/FacilityController.java
@@ -10,18 +10,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/facility/*")
 public class FacilityController {
 
+	// http://localhost:8088/facility/list
 	@RequestMapping(value = "/list")
 	public void facilityListGET() {
 		// 설비 목록 return
 	}
 
-	@RequestMapping(value = "/insert", method = RequestMethod.GET)
+	// http://localhost:8088/facility/fac-insert
+	@RequestMapping(value = "/fac-insert", method = RequestMethod.GET)
 	public void facilityInsertGET() {
 		// 설비 추가 폼
 	}
 	
-	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	@RequestMapping(value = "/facility-insert", method = RequestMethod.POST)
 	public void facilityInsertPOST() {
 		// 설비 추가 액션
 	}
+	
+	@RequestMapping(value = "/facility-update", method = RequestMethod.GET)
+	public void facilityUpdateGET() {
+		// 설비 수정 폼
+	}
+	
+	@RequestMapping(value = "/facility-update", method = RequestMethod.POST)
+	public void facilityUpdatePOST() {
+		// 설비 수정 액션
+	}
+	
+	@RequestMapping(value = "/facility-delete", method = RequestMethod.GET)
+	public void facilityDeleteGET() {
+		// 설비 삭제 폼
+	}
+	
+	@RequestMapping(value = "/facility-delete", method = RequestMethod.POST)
+	public void facilityDeletePOST() {
+		// 설비 삭제 액션
+	}
+	
+	@RequestMapping(value = "/maintenance-list", method = RequestMethod.GET)
+	public void facilityMaintenanceListGET() {
+		// 설비 보전 리스트
+	}
+	
+	
 }
