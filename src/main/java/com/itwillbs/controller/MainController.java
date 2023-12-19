6164cabcb831a2695feaf59d43af37e8d99e9f6d
sumin901 <klop1211@naver.com>
2023-12-19 16:51:04 +0900

    loginTest

--- src/main/java/com/itwillbs/controller/MainController.java
+++ src/main/java/com/itwillbs/controller/MainController.java
@@ -8,13 +8,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /** 메인 페이지 **/
 
 @Controller
-@RequestMapping(value = "/main", method = RequestMethod.GET)
+@RequestMapping(value = "/main/*", method = RequestMethod.GET)
 public class MainController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -30,4 +32,12 @@ public class MainController {
 		model.addAttribute("serverTime", formattedDate );
 		return "/main/main";
 	}
+	
+	// http://localhost:8088/main/login
+	@GetMapping (value = "/login")
+	public void loginGET() {
+		logger.info("cc : loginGET() 실행");
+	}
+	
+	
 }
